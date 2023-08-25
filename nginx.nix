727e97451b0dd84346dc950ca6f654e1e3e3348e
Kasper Gałkowski <kgalkowski@comscore.com>
2023-08-25 18:28:41 +0200

    tweaks

--- nginx.nix
+++ nginx.nix
@@ -1,31 +1,22 @@
 { pkgs, dockerTools, buildEnv, ... }:
 
 let
-  mkdir = name: pkgs.runCommand name { inherit name; } ''
-    mkdir -p $out/$name
-    chmod a+w $out/$name
-  '';
   conf = pkgs.writeText "nginx.conf" ''
-    worker_processes  1;
-
     daemon off;
-
-    error_log /dev/null;
 
     pid /dev/null;
 
     events {
-      worker_connections  1024;
     }
 
     http {
-      access_log /dev/null;
+      access_log off;
       server {
         listen       80;
         server_name  localhost;
         location / {
             root   ${pkgs.nginx}/html;
-            index  index.html index.htm;
+            index  index.html;
         }
       }
     }
@@ -49,8 +40,8 @@ in dockerTools.buildImage {
   };
 
   extraCommands = ''
-    mkdir tmp var
-    chmod a+w tmp var
+    mkdir tmp
+    chmod a+w tmp
   '';
 
   config = {
